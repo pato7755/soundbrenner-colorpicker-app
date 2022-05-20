3fd786d99f97737fe88a80faaa74bfb69d9a9d09
Patrick Essiam <pat.essiam701@gmail.com>
2022-05-20 20:46:58 +0000

    First commit

---
+++ app/src/main/java/com/soundbrenner/colorpicker/ui/main/MainFragment.kt
@@ -0,0 +1,39 @@
+package com.soundbrenner.colorpicker.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.soundbrenner.colorpicker.databinding.MainFragmentBinding
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/soundbrenner/colorpicker/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.soundbrenner.colorpicker.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

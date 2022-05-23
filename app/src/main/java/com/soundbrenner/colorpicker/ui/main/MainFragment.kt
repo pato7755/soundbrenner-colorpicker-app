ab1e9b8496ef6fdabf7b4f62902a294b8e44d903
Patrick Essiam <pat.essiam701@gmail.com>
2022-05-23 05:08:55 +0000

    Add touch listener for color wheel

--- app/src/main/java/com/soundbrenner/colorpicker/ui/main/MainFragment.kt
+++ app/src/main/java/com/soundbrenner/colorpicker/ui/main/MainFragment.kt
@@ -1,10 +1,15 @@
 package com.soundbrenner.colorpicker.ui.main
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.soundbrenner.colorpicker.databinding.MainFragmentBinding
 
@@ -18,11 +23,33 @@ class MainFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var viewModel: MainViewModel
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = MainFragmentBinding.inflate(inflater, container, false)
+
+        var bitmap: Bitmap
+
+
+        binding.imageView.setOnTouchListener{ view: View, event: MotionEvent ->
+            if (event.action == MotionEvent.ACTION_DOWN || event.action == MotionEvent.ACTION_MOVE){
+                bitmap = viewModel.getBitmapFromView(binding.imageView)
+
+                val selectedColor = viewModel.getSelectedColor(bitmap, event.x.toInt(), event.y.toInt())
+
+                println("selectedColor: $selectedColor")
+
+
+//                Toast.makeText(context, selectedColor, Toast.LENGTH_SHORT).show()
+
+//                view.setBackgroundColor(Color.rgb(r, g, b))
+
+            }
+            true
+        }
+
         return binding.root
     }
 
@@ -36,4 +63,5 @@ class MainFragment : Fragment() {
         super.onDestroy()
         _binding = null
     }
+
 }
--- app/src/main/java/com/soundbrenner/colorpicker/ui/main/MainViewModel.kt
+++ app/src/main/java/com/soundbrenner/colorpicker/ui/main/MainViewModel.kt
@@ -1,7 +1,39 @@
 package com.soundbrenner.colorpicker.ui.main
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.view.View
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val pixels = MutableLiveData<Int>()
+
+    fun getBitmapFromView(view: View): Bitmap {
+        var bitmap =
+            Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        var canvas = Canvas(bitmap)
+        view.draw(canvas)
+        return bitmap
+    }
+
+    fun getSelectedColor(bitmap: Bitmap, x: Int, y: Int): String {
+        val pixel = bitmap.getPixel(x, y)
+
+        val r = pixel.let { Color.red(it) }
+        val g = pixel.let { Color.green(it) }
+        val b = pixel.let { Color.blue(it) }
+
+        val hex = "#".plus(Integer.toHexString(pixel))
+
+        return hex
+
+//        return Color.rgb(r, g, b)
+
+    }
+
+
+
 }
